6dbee2a779321f9fc1e9ccd6fe47dc72779d4785
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-21 17:08:33 -0500

    Add runtimeConfig option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,12 @@ export default defineNuxtConfig({
       },
     ],
   ],
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api',
+    },
+  },
 });
