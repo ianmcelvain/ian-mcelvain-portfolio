fdb41bd3ef893eb02f3579d11f71387cc64a302b
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-23 10:39:34 -0500

    Refactor index to use layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavBar />
+    <PageContainer>
+      <ReadingContainer>
+        <slot />
+      </ReadingContainer>
+    </PageContainer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
