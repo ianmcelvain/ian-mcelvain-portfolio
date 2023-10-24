9823c639aeee3d976d2fbdf2a8c2371275281e82
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-24 16:46:09 -0500

    Add filterable projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -4,20 +4,51 @@
 
     <div class="project-grid">
       <FeatureCard
-        image="https://cdn.sanity.io/images/exj3bhzf/production/736302c62d76b32c9c2e7ebe1f4ff01bf1549f8c-1198x759.jpg"
-        title="Balloon Fight"
-        description="Fight balloons with swords, arrows, guns, staple gun...?"
-      />
-      <FeatureCard
-        image="https://cdn.sanity.io/images/exj3bhzf/production/736302c62d76b32c9c2e7ebe1f4ff01bf1549f8c-1198x759.jpg"
-        title="Balloon Fight"
-        description="Fight balloons with swords, arrows, guns, staple gun...?"
+        v-for="project in filteredProjects"
+        :key="project.title"
+        :image="project.image"
+        :title="project.title"
+        :description="project.description"
       />
     </div>
   </div>
 </template>
 
 <script setup>
+const projectData = [
+  {
+    title: 'Balloon Fight',
+    image:
+      'https://cdn.sanity.io/images/exj3bhzf/production/736302c62d76b32c9c2e7ebe1f4ff01bf1549f8c-1198x759.jpg',
+    description: 'Fight balloons with swords, arrows, guns, staple gun...?',
+    category: 'game',
+  },
+  {
+    title: 'Cardinal Financial',
+    image:
+      'https://cdn.sanity.io/images/exj3bhzf/production/37b705f13c364d0599bb66aaf3d2ee717299a176-1905x943.png',
+    description:
+      'Collaborated with designers as a full stack engineer to build Cardinal Financials site during their rebranding.',
+    category: 'website',
+  },
+  {
+    title: 'FooMo',
+    image:
+      'https://cdn.sanity.io/images/exj3bhzf/production/3b2c89fdf4956c53a6dbb0d4461f9d0ae5514b3b-752x458.png',
+    description:
+      'Stalk your favorite food trucks with real-time updates and earn experience visiting them to unlock coupons.',
+    category: 'app',
+  },
+  {
+    title: 'The Programmer v2',
+    image:
+      'https://cdn.sanity.io/images/exj3bhzf/production/736302c62d76b32c9c2e7ebe1f4ff01bf1549f8c-1198x759.jpg',
+    description:
+      'Collaborated as the front end developer for a stock app that would display and predict stocks using JS and Python.',
+    category: 'other',
+  },
+];
+
 const categories = ref([
   {
     name: 'All',
@@ -37,8 +68,17 @@ const categories = ref([
   },
 ]);
 
-function handleActiveChange(item) {
-  // TODO: filter projects here
+const filteredProjects = ref(projectData);
+
+function handleActiveChange(category) {
+  if (category.name === 'All') {
+    filteredProjects.value = projectData;
+    return;
+  }
+
+  filteredProjects.value = projectData.filter((project) => {
+    return project.category === category.name.toLowerCase();
+  });
 }
 </script>
 
