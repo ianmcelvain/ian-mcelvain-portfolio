8f0941362df5bffd0e1a2d0d5180e33fb0365ce3
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-23 20:35:18 -0500

    Add IconButtonGroup component

---
+++ components/IconButtonGroup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="`icon-button-group ${orientation}`">
+    <IconButton
+      v-for="item in items"
+      :key="item.name"
+      :icon="item.icon"
+      :text="item.name"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+  orientation: {
+    type: String,
+    default: 'horizontal',
+  },
+});
+</script>
+
+<style scoped>
+.icon-button-group.horizontal {
+  @apply flex;
+}
+</style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <h1>PROJECTS</h1>
+  <IconButtonGroup :items="categories" />
 </template>
 
-<script setup></script>
+<script setup>
+const categories = ref([
+  {
+    name: 'All',
+    icon: 'mdi:layers-triple-outline',
+  },
+  {
+    name: 'Game',
+    icon: 'mdi:controller-classic-outline',
+  },
+  {
+    name: 'App',
+    icon: 'mdi:cellphone',
+  },
+  {
+    name: 'Website',
+    icon: 'mdi:web',
+  },
+]);
+</script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
