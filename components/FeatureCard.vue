24fe81c2af34f3450aa6f144d27ca51813e9c941
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-24 15:14:02 -0500

    Add FeatureCard for project grid

---
+++ components/FeatureCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="feature">
+    <div
+      class="feature-image"
+      :style="{
+        backgroundImage: `url(${image})`,
+      }"
+    ></div>
+    <NuxtLink to="/" class="feature-link">
+      <Icon
+        :icon="'mdi:controller-classic-outline'"
+        :width="26"
+        class="flex-none mt-1.5 mr-2.5"
+      />
+      <div>
+        <h3 class="text-gray-800">{{ title }}</h3>
+        <div class="mb-0 text-sm text-gray-500">
+          <p>{{ description }}</p>
+        </div>
+        <div class="flex">
+          <span
+            class="whitespace-nowrap w-fit rounded-lg bg-gray-100 px-2.5 py-0.5 text-sm text-gray-700 lg:m-0"
+          >
+            Archived
+          </span>
+          <Icon :icon="'mdi:web'" :width="20" class="mt-2 ml-auto" />
+        </div>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue';
+
+defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.feature {
+  @apply mt-12;
+}
+.feature-image {
+  @apply h-56 -mt-8 bg-cover bg-center rounded-xl relative shadow-md;
+}
+.feature-link {
+  @apply relative flex bg-white -mt-8 shadow-lg mx-8 -mt-8 rounded-xl p-3 
+          transition-all duration-300 transform-gpu translate-y-0 
+          hover:shadow-xl hover:-translate-y-1 ease-in-out;
+}
+</style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <IconButtonGroup :items="categories" @active-change="handleActiveChange" />
+  <div>
+    <IconButtonGroup :items="categories" @active-change="handleActiveChange" />
+
+    <div class="project-grid">
+      <FeatureCard
+        image="https://cdn.sanity.io/images/exj3bhzf/production/736302c62d76b32c9c2e7ebe1f4ff01bf1549f8c-1198x759.jpg"
+        title="Balloon Fight"
+        description="Fight balloons with swords, arrows, guns, staple gun...?"
+      />
+      <FeatureCard
+        image="https://cdn.sanity.io/images/exj3bhzf/production/736302c62d76b32c9c2e7ebe1f4ff01bf1549f8c-1198x759.jpg"
+        title="Balloon Fight"
+        description="Fight balloons with swords, arrows, guns, staple gun...?"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -27,4 +42,9 @@ function handleActiveChange(item) {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.project-grid {
+  @apply grid gap-12 my-8 grid-cols-1 
+          sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4;
+}
+</style>
