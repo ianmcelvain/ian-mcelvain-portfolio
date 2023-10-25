9385cc5a168d4e2f567db40c2512d86d18970e2a
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-25 09:16:18 -0500

    Add updates

--- pages/updates/index.vue
+++ pages/updates/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <h1>UPDATES</h1>
+  <div>
+    <IconButtonGroup
+      :items="UPDATE_CATEGORIES"
+      @active-change="handleActiveChange"
+    />
+    <Grid>
+      <BlogCard
+        title="CMD + A + DEL Everything & Add AI"
+        image="https://cdn.sanity.io/images/exj3bhzf/production/f471dfa5992fe2619703331df388513b1c316f17-785x496.jpg"
+        excerpt="Better animations, better systems, Papa Johns's..."
+        category="game"
+        tag="Balloon Fight"
+        publish-date="February 12th 2023"
+      />
+    </Grid>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import UPDATE_CATEGORIES from '~/constants/update_categories';
+
+function handleActiveChange(category) {}
+</script>
 
 <style scoped></style>
