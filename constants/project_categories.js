11ecac9b8f9a4002b6c074d9c91ad792235bc1ea
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-25 09:15:04 -0500

    Add placeholder constants until CMS

---
+++ constants/project_categories.js
@@ -0,0 +1,20 @@
+const PROJECT_CATEGORIES = Object.freeze([
+  {
+    name: 'All',
+    icon: 'mdi:layers-triple-outline',
+  },
+  {
+    name: 'Game',
+    icon: 'mdi:controller-classic-outline',
+  },
+  {
+    name: 'App',
+    icon: 'mdi:cellphone',
+  },
+  {
+    name: 'Website',
+    icon: 'mdi:web',
+  },
+]);
+
+export default PROJECT_CATEGORIES;
---
+++ constants/update_categories.js
@@ -0,0 +1,20 @@
+const UPDATE_CATEGORIES = Object.freeze([
+  {
+    name: 'All',
+    icon: 'mdi:layers-triple-outline',
+  },
+  {
+    name: 'Development',
+    icon: 'mdi:source-branch',
+  },
+  {
+    name: 'Release',
+    icon: 'mdi:tray-arrow-down',
+  },
+  {
+    name: 'Uncategorized',
+    icon: 'mdi:file-outline',
+  },
+]);
+
+export default UPDATE_CATEGORIES;
