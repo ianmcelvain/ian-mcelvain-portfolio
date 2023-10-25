5cb33793b1053762ee1980851d9d6614bfaa1e46
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-25 13:40:12 -0500

    Edit margins

--- components/Grid.vue
+++ components/Grid.vue
@@ -6,7 +6,7 @@
 
 <style scoped>
 .grid-wrapper {
-  @apply grid gap-12 my-8 grid-cols-1 
+  @apply grid gap-12 mb-8 grid-cols-1 
           sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4;
 }
 </style>
