e157edbf6c03ad2dbda4c0bd596cb65ea76fd2d7
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-30 13:17:10 -0500

    Add vue3-toastify

---
+++ plugins/vue3-toastify.js
@@ -0,0 +1,16 @@
+import { toast, updateGlobalOptions } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.config.globalProperties.Vue3Toastify = {
+    install: () => {
+      updateGlobalOptions({ autoClose: 1000 });
+    },
+  };
+
+  return {
+    provide: {
+      toast,
+    },
+  };
+});
