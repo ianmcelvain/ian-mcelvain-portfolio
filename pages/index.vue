c5360ea47290c250965c9527eadfbaa7e1aeb225
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-29 20:02:23 -0500

    Edit bio

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <ArticleContainer class="text-center">
     <h4 className="subtitle">
-      I've been writing games, native apps, and websites since 2012
+      Writing games, native apps, and websites since 2012
     </h4>
     <p>
-      I started by developing mods for Call of Duty using C++ and it naturally
-      transitioned into developing games shortly after. Since then games have
-      been my main focus but in parallel I use those software principles and
-      patterns for apps and the web.
+      I started by making mods for Call of Duty using C++ which transitioned
+      into designing games shortly after. Since then I've been working on games
+      whilst applying those software principles and patterns for apps and the
+      web.
     </p>
     <div className="flex my-8 justify-center items-center">
       <a href="mailto:ian.mcelvain@gmail.com">
