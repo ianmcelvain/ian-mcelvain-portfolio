ef9336f2f685e6201d2d33129a7857ae07c487d0
Ian McElvain <ian.mcelvain@gmail.com>
2023-12-02 11:48:27 -0600

    Clean update card

--- components/UpdateCard.vue
+++ components/UpdateCard.vue
@@ -1,51 +1,31 @@
 <template>
-  <NuxtLink
-    :to="`/updates/${slug}`"
-    class="text-gray-800 relative justify-between rounded-xl p-4 shadow-lg transition-all hover:shadow-xl p-2 duration-300 tranform-gpu translate-y-0 hover:-translate-y-1 ease-in-out"
-  >
+  <NuxtLink :to="`/updates/${slug}`" class="update">
     <div
-      class="post-thumbnail bg-center bg-cover h-60 mb-4 relative"
+      class="image"
       :style="{
         backgroundImage: `url(${featuredImage.url})`,
       }"
     >
-      <div
-        class="absolute flex font-medium drop-shadow-lg bottom-0 left-0 -mb-5 ml-3"
-      >
-        <span
-          :class="`text hover ${category.slug}  whitespace-nowrap rounded-2xl bg-white px-2.5 py-2.5 text-sm mr-2 cursor-pointer transition-all duration-300 hover:bg-gray-700 hover:text-white tranform-gpu translate-y-0 hover:-translate-y-1 ease-in-out`"
-        >
-          <Icon :icon="category.icon" :width="20" />
-        </span>
-        <div
-          href="/"
-          class="whitespace-nowrap rounded-2xl bg-white px-2.5 py-2.5 text-sm text-gray-700 mr-2 cursor-pointer transition-all duration-300 hover:bg-gray-700 hover:text-white tranform-gpu translate-y-0 hover:-translate-y-1 ease-in-out"
-        >
-          <Icon icon="feather:message-square" :width="20" />
-        </div>
+      <div class="meta-details">
+        <FloatingIcon :icon="category.icon" />
+        <FloatingIcon icon="feather:message-square" />
       </div>
     </div>
-    <div class="mt-10">
+    <div class="details">
       <div class="text-xs">
         {{ format(new Date(publishedAt), 'MMMM do yyyy') }}
       </div>
       <h3>{{ title }}</h3>
       <p>{{ excerpt }}</p>
-      <span
-        v-if="tag"
-        class="whitespace-nowrap rounded-lg bg-gray-100 px-2.5 py-0.5 text-sm text-gray-700"
-      >
-        {{ tag }}
-      </span>
+      <Tag v-for="tag in tags" :key="tag.slug">{{ tag.title }}</Tag>
     </div>
   </NuxtLink>
 </template>
 
 <script setup>
-import { Icon } from '@iconify/vue';
 import { format } from 'date-fns';
 
-const props = defineProps({
+defineProps({
   title: {
     type: String,
     required: true,
@@ -62,9 +42,9 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  tag: {
-    type: String,
-    default: '',
+  tags: {
+    type: Array,
+    default: () => [],
   },
   category: {
     type: Object,
@@ -80,4 +60,18 @@ const props = defineProps({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.update {
+  @apply text-gray-800 relative justify-between rounded-xl p-4 shadow-lg hover:shadow-xl p-2 
+          transition-all duration-300 transform-gpu translate-y-0 hover:-translate-y-1 ease-in-out;
+}
+.image {
+  @apply bg-cover bg-center h-60 mb-4 relative;
+}
+.meta-details {
+  @apply absolute flex font-medium drop-shadow-lg bottom-0 left-0 -mb-5 ml-3;
+}
+.details {
+  @apply mt-10;
+}
+</style>
