6b19c3ab91ab9c41ee7bf970dc322536e1ebe99d
Ian McElvain <ian.mcelvain@gmail.com>
2023-12-29 13:19:09 -0600

    Use prose-primary theme

--- components/layout/ArticleContainer.vue
+++ components/layout/ArticleContainer.vue
@@ -6,7 +6,7 @@
 
 <style>
 article {
-  @apply prose prose-slate mx-auto mb-32;
+  @apply prose prose-primary mx-auto mb-32;
 }
 /* Hack to counter image margins so that em tags look like they at image captions */
 article > p > em {
