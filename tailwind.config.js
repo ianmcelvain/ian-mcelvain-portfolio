83a3e8c771ead6a39b46824735dc884ca5dad037
Ian McElvain <ian.mcelvain@gmail.com>
2024-01-02 15:08:51 -0600

    Add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter'],
+      },
       typography: ({ theme }) => ({
         primary: {
           css: {
